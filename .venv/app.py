0190bb20771391ba00d71699f3ee1388e900a536
Joshua Rodriguez <joshrod1089@gmail.com>
2025-03-22 00:41:58 +0000

    - Takes a screenshot of video stream every 1 second - Sends that image over to server via post request - Started handling json file on server end

--- .venv/app.py
+++ .venv/app.py
@@ -1,11 +1,11 @@
 import Smile as Backend
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static", static_folder="static")
 
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
@@ -15,6 +15,15 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html", title="Home")
 
+@app.route('/sendImage', methods=['POST'])
+def sent():
+    if request.method == "POST":
+        # Take payload, convert it to image we're expecting
+        request_data = request.get_json()
+        print(request_data)
+        # Run through backend
+        # For now, save images made in folder
+    return "hello world"
 # main driver function
 if __name__ == '__main__':
 
